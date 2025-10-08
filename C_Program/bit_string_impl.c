5b686ebe6d9b9ca7a7042417aa99881c41de9067
Rocky01-dev <dev.adithyan0001@gmail.com>
2025-10-08 12:12:29 +0530

    fixed error

--- C_Program/bit_string_impl.c
+++ C_Program/bit_string_impl.c
@@ -1,134 +1,133 @@
 #include <stdio.h>
-
 void input();
-void output(int c[]);
+void output();
 void setunion();
 void intersection();
 void difference();
-
-int a[9] = {0};
-int b[9] = {0};
-
+int a[]={0,0,0,0,0,0,0,0,0},b[]={0,0,0,0,0,0,0,0,0};
 int main()
 {
-    int ch, wish;
-    do
-    {
-        printf("\nMENU\n");
-        printf("1. Input\n2. Union\n3. Intersection\n4. Difference\n");
-        printf("Enter choice: ");
-        scanf("%d", &ch);
-        switch(ch)
-        {
-            case 1:
-                input();
-                break;
-            case 2:
-                setunion();
-                break;
-            case 3:
-                intersection();
-                break;
-            case 4:
-                difference();
-                break;
-            default:
-                printf("Invalid choice!\n");
-        }
-        printf("\nDo you wish to continue? (1/0): ");
-        scanf("%d", &wish);
-    } while(wish == 1);
-    
-    return 0;
-}
+int ch,wish;
+do
+{
+printf("\n MENU \n");
+printf("1.Input\n2.Union\n3.Intersection\n4.Difference\n");
+printf("Enter choice\n");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:input();
+break;
+case 2:setunion();
+break;
+case 3:intersection();
+break;
+case 4:difference();
 
+
+
+break;
+}
+printf("\nDo you wish to continue ?(1/0)\n");
+scanf("%d",&wish);
+} while(wish==1);
+}
 void input()
 {
-    int n, x, i;
-    
-    // Reset arrays before new input
-    for(i = 0; i < 9; i++) {
-        a[i] = 0;
-        b[i] = 0;
-    }
-    
-    printf("Enter size of the 1st set: ");
-    scanf("%d", &n);
-    printf("Enter elements (1 to 9): ");
-    for(i = 0; i < n; i++)
-    {
-        scanf("%d", &x);
-        if(x >= 1 && x <= 9)
-            a[x - 1] = 1;
-        else
-            printf("Invalid element ignored: %d\n", x);
-    }
-    
-    printf("Enter size of the 2nd set: ");
-    scanf("%d", &n);
-    printf("Enter elements (1 to 9): ");
-    for(i = 0; i < n; i++)
-    {
-        scanf("%d", &x);
-        if(x >= 1 && x <= 9)
-            b[x - 1] = 1;
-        else
-            printf("Invalid element ignored: %d\n", x);
-    }
-    
-    printf("\n1st set: ");
-    output(a);
-    
-    printf("\n2nd set: ");
-    output(b);
-    printf("\n");
+int n,x,i;
+printf("enter size of the 1st set\n");
+scanf("%d",&n);
+printf("enter elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&x);
+a[x-1]=1;
+}
+printf("enter size of the 2nd set\n");
+scanf("%d",&n);
+printf("enter elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&x);
+b[x-1]=1;
+}
+printf("\n1st set\n");
+for(i=0;i<9;i++)
+{
+printf("%d",a[i]);
+}
+
+
+
+printf("\n2nd set\n");
+for(i=0;i<9;i++)
+{
+printf("%d",b[i]);
+}
 }
 
 void output(int c[])
 {
-    int i;
-    printf("{ ");
-    for(i = 0; i < 9; i++)
-    {
-        if (c[i] != 0)
-            printf("%d ", i + 1);
-    }
-    printf("}");
-}
+int i;
+printf("\n Set is ");
+for(i=0;i<9;i++)
+{
+if (c[i]!=0)
+printf("%d ",i+1);
 
+}
+}
 void setunion()
 {
-    int i, c[9];
-    for(i = 0; i < 9; i++)
-    {
-        c[i] = (a[i] || b[i]) ? 1 : 0;
-    }
-    printf("\nUnion of sets: ");
-    output(c);
-    printf("\n");
-}
+int i,c[10];
+for(i=0;i<9;i++)
+{
+if (a[i]!=b[i])
+c[i]=1;
+else
+c[i]=a[i];
 
+}
+for(i=0;i<9;i++)
+{
+printf("%d",c[i]);
+
+
+}
+output(c);
+}
 void intersection()
 {
-    int i, c[9];
-    for(i = 0; i < 9; i++)
-    {
-        c[i] = (a[i] && b[i]) ? 1 : 0;
-    }
-    printf("\nIntersection of sets: ");
-    output(c);
-    printf("\n");
-}
+int i,c[10];
+for(i=0;i<9;i++)
+{
+if (a[i]==b[i])
+c[i]=a[i];
+else
+c[i]=0;
 
+}
+for(i=0;i<9;i++)
+{
+printf("%d",c[i]);
+}
+output(c);
+}
 void difference()
 {
-    int i, c[9];
-    for(i = 0; i < 9; i++)
-    {
-        c[i] = (a[i] == 1 && b[i] == 0) ? 1 : 0;
-    }
-    printf("\nDifference (1st set - 2nd set): ");
-    output(c);
-    printf("\n");
+int i,c[10];
+for(i=0;i<9;i++)
+{
+if (a[i]==1 && b[i]==0)
+c[i]=1;
+else
+c[i]=0;
+
 }
 
+for(i=0;i<9;i++)
+{
+printf("%d",c[i]);
+}
+output(c);
+}
